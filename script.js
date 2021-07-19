66185b6181b7e4fe254abf3143b4f76276e875a3
ryan-cariaso <ryanchristophercariaso@gmail.com>
2021-07-19 00:04:42 -0700

    final edit on html and css file

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function calculateTip() {
   var bill = document.getElementById("bill").value;
   var tip = document.getElementById("tip").value;
-  
+  var numOfPeople = document.getElementById("numOfPeople").value;
+
+  //calculate tip and round two decimal places
+  var total = (bill * tip) / numOfPeople;
+  total = Math.round(total * 100) / 100;
+  total = total.toFixed(2);
+
+  document.getElementById("tipAmtResults").style.display = "block";
+  document.getElementById("totalAmt").innerHTML = total;
+
 }
